aa7bd01c8740e01e55b2e4ee49319ff44a8a7136
Ezek10 <ezequielmarcel2@gmail.com>
2024-10-15 04:06:47 -0300

    add color random generator

--- src/main/dto/supplier_dto.py
+++ src/main/dto/supplier_dto.py
@@ -1,9 +1,12 @@
 from typing import Optional
 from pydantic import BaseModel, ConfigDict, Field
+import random
 
 
 def random_color_generator():
-    return "#B4321"
+    color = random.randrange(0, 2**24)
+    hex_color = hex(color)
+    return "#" + hex_color[2:]
 
 class Supplier(BaseModel):
     model_config = ConfigDict(from_attributes=True, str_strip_whitespace=True, str_to_upper=True, use_enum_values=True)
